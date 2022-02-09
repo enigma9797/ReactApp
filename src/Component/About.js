98e3b76d1bfd2ab1321585e788295db06a6afc52
Akash Dixit <akash9797@hotmail.com>
2022-02-09 19:23:30 +0530

    masterlATEST

--- src/Component/About.js
+++ src/Component/About.js
@@ -58,7 +58,7 @@ const changeStyle = ()=>{
   <div className="accordion-item">
     <h2 className="accordion-header"  id="headingThree">
       <button className="accordion-button collapsed" style={modeStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+        Accordion Item #4
       </button>
     </h2>
     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
